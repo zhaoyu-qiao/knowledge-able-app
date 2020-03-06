4727288bf75f0b89bb20ca2b9079b104e9ac6ea7
Zhaoyu Qiao <qiao.zy7@gmail.com>
2020-03-05 21:38:24 -0500

    modify note model

--- models/note.js
+++ models/note.js
@@ -4,15 +4,15 @@ const Schema = mongoose.Schema;
 const noteSchema = new Schema({
   title: { type: String },
   link: {
-    type: String,
-    validator: [
-      validators.isURL({
-        message: "Must be a Valid URL",
-        protocols: ["http", "https", "ftp"],
-        require_tld: true,
-        require_protocol: true
-      })
-    ]
+    type: String
+    // validator: [
+    //   validators.isURL({
+    //     message: "Must be a Valid URL",
+    //     protocols: ["http", "https", "ftp"],
+    //     require_tld: true,
+    //     require_protocol: true
+    //   })
+    // ]
   },
   comment: { type: String }
 });
