1ce188064314edfd2b10f443bd7b2faf7a3d509e
Zhaoyu Qiao <qiao.zy7@gmail.com>
2020-03-06 19:01:17 -0500

    add auth log

--- client/src/components/Note/index.js
+++ client/src/components/Note/index.js
@@ -1,130 +1,140 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Input, FormBtn } from "../GeneralForm/index";
 import NoteAPI from "../../utils/NoteAPI";
 import { Modal, Button } from "react-bootstrap";
 
-class Note extends Component {
-  state = {
-    notes: [],
-    note: {},
-    comment: "",
-    title: "",
-    link: ""
-  };
+// functional component with hooks
+function Note(props) {
+  // Modal show/hide
+  const [show, setShow] = useState(false);
+  const [message, setMessage] = useState("Your Note is Saved!");
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  handleInputChange = event => {
-    console.log("Handle Input");
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-    console.log(this.state);
-  };
+  // Other states
+  const [title, setTitle] = useState("");
+  const [comment, setComment] = useState("");
+  const [link, setLink] = useState("");
 
-  handleFormSubmit = event => {
+  const handleFormSubmit = event => {
     event.preventDefault();
-    console.log("Form submitted", this.state.title);
+    console.log("Form submitted", title);
     const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
     const regex = new RegExp(expression);
-    if (this.state.link.match(regex)) {
+    if (link.match(regex)) {
       NoteAPI.saveNote({
-        title: this.state.title,
-        link: this.state.link,
-        comment: this.state.comment
+        title: title,
+        link: link,
+        comment: comment
       })
         .then(res => {
-          alert("Your note is saved!");
+          setMessage("Your note is saved!");
+          handleShow();
           console.log("response", res);
-          this.setState({
-            note: res || ["no results"],
-            comment: res.comment,
-            title: res.title,
-            link: res.link
-          });
+
+          setTitle(res.title);
+          setComment(res.comment);
+          setLink(res.link);
         })
         .catch(err => console.log(err));
     } else {
-      alert("Please insert a valid url.:)");
+      // show Modal
+      setMessage("Please insert a valid url.:)");
+      handleShow();
     }
   };
 
-  // handle input
-  // hanele submit
-
-  render() {
-    return (
-      <div style={{ overflow: "scroll", maxHeight: "400" + "px" }}>
-        <div>
-          <h4 className="text-info"> Put your notes here </h4>
-          <form>
-            <b>Title:</b>
-            <Input
-              id="title"
-              type="text"
-              value={this.state.title}
-              onChange={this.handleInputChange}
-              name="title"
-              placeholder="Your note title"
-            />
-            <b>Link:</b>
-            <Input
-              id="link"
-              type="text"
-              value={this.state.link}
-              onChange={this.handleInputChange}
-              name="link"
-              placeholder="Put the link you like here"
-            />
-            <b>Comment:</b>
-            <Input
-              id="comment"
-              type="text"
-              value={this.state.comment}
-              onChange={this.handleInputChange}
-              name="comment"
-              placeholder="Comment here"
-            />
-            <FormBtn
-              disabled={!this.state.title || !this.state.comment}
-              onClick={this.handleFormSubmit}
-            >
-              Submit{" "}
-            </FormBtn>{" "}
-          </form>{" "}
-        </div>
-        <hr />
-        <div>
-          <h4
-            className="text-info"
-            style={{
-              marginTop: 20 + "px",
-              paddingTop: 10 + "px",
-              textAlign: "left"
-            }}
-          >
-            Your Current Note:
-          </h4>
-          <div>
-            <p>
-              <b>Title: </b>
-              {this.state.title}
-            </p>
-            <p>
-              <b>Link: </b>
-              {this.state.link}
-            </p>
-            <p>
-              <b>Comment: </b>
-              {this.state.comment}
-            </p>
-          </div>
-          <a href="/notes" class="button">
-            Check all notes here
-          </a>
-        </div>
+  return (
+    <div style={{ overflow: "scroll", maxHeight: "400" + "px" }}>
+      <div>
+        <h4 className="text-info"> Put your notes here </h4>
+        <form>
+          <b>Title:</b>
+          <Input
+            id="title"
+            type="text"
+            // value={this.state.title}
+            value={title}
+            // onChange={this.handleInputChange}
+            onChange={e => setTitle(e.target.value)}
+            name="title"
+            placeholder="Your note title"
+          />
+          <b>Link:</b>
+          <Input
+            id="link"
+            type="text"
+            // value={this.state.link}
+            value={link}
+            onChange={e => setLink(e.target.value)}
+            // onChange={handleInputChange}
+            name="link"
+            placeholder="Put the link you like here"
+          />
+          <b>Comment:</b>
+          <Input
+            id="comment"
+            type="text"
+            // value={state.comment}
+            value={comment}
+            onChange={e => setComment(e.target.value)}
+            name="comment"
+            placeholder="Comment here"
+          />
+          <FormBtn disabled={!title || !comment} onClick={handleFormSubmit}>
+            Submit{" "}
+          </FormBtn>{" "}
+        </form>{" "}
       </div>
-    );
-  }
+      <div>
+        <Modal
+          show={show}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          // onClick={handleClose}
+        >
+          <Modal.Body>
+            <span style={{ float: "left" }}>{message}</span>
+            <Button style={{ float: "right" }} onClick={handleClose}>
+              {" "}
+              Close{" "}
+            </Button>
+          </Modal.Body>{" "}
+        </Modal>
+      </div>
+      <hr />
+      <div>
+        <h4
+          className="text-info"
+          style={{
+            marginTop: 20 + "px",
+            paddingTop: 10 + "px",
+            textAlign: "left"
+          }}
+        >
+          Your Current Note:
+        </h4>
+        <div>
+          <p>
+            <b>Title: </b>
+            {title}
+          </p>
+          <p>
+            <b>Link: </b>
+            {link}
+          </p>
+          <p>
+            <b>Comment: </b>
+            {comment}
+          </p>
+        </div>
+        <a href="/notes" class="button">
+          Check all notes here
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Note;
