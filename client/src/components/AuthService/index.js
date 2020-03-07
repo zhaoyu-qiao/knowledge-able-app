4d4d0e54b3544c925fd310f696ee11d139aa74e6
Zhaoyu Qiao <qiao.zy7@gmail.com>
2020-03-07 14:57:04 -0500

    add todos, modify model

--- client/src/components/AuthService/index.js
+++ client/src/components/AuthService/index.js
@@ -3,12 +3,15 @@ import axios from "axios";
 export default class AuthService {
   login = (username, password) => {
     // Get a token
+
     return axios
       .post("/api/users/login", {
         username: username,
         password: password
       })
       .then(res => {
+        // TODO: save username to localStorage?
+        console.log(res.data);
         // set the token once the user logs in
         this.setToken(res.data.token);
         // return the rest of the response
@@ -47,6 +50,11 @@ export default class AuthService {
     // Retrieves the user token from localStorage
     return localStorage.getItem("id_token");
   }
+
+  // TODO:set and get of username
+  setUsername() {}
+
+  getUsername() {}
 
   logout() {
     // Clear user token and profile data from localStorage
